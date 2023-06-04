bdd970777db24638c1b499f9249507b715df8402
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 14:43:33 +0900

    feat: todos 一覧を表示する view を用意

--- src/app/pds-cqs/views/todo-list-view/todo-list-view.component.ts
+++ src/app/pds-cqs/views/todo-list-view/todo-list-view.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../../todo';
 
 @Component({
   selector: 'app-todo-list-view',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './todo-list-view.component.html',
-  styleUrls: ['./todo-list-view.component.scss']
+  styleUrls: ['./todo-list-view.component.scss'],
 })
 export class TodoListViewComponent {
+  @Input() todos: Todo[] = [];
 
+  @Output() changeCompleted = new EventEmitter<{
+    id: Todo['id'];
+    completed: boolean;
+  }>();
 }
