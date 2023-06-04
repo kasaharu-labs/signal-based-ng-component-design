c52fc48250ddacc7a22821f594eb0f28e01997d3
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 14:33:25 +0900

    npm run ng g c pds-cqs/views/todo-list-view

---
+++ src/app/pds-cqs/views/todo-list-view/todo-list-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-list-view.component.html',
+  styleUrls: ['./todo-list-view.component.scss']
+})
+export class TodoListViewComponent {
+
+}
