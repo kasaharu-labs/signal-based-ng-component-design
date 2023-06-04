aa3f248920b7a124cd69d94e5d3a042b4064ef48
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 15:20:27 +0900

    test: Testing Library を使って view のテストを用意

--- src/app/pds-cqs/views/todo-list-view/todo-list-view.component.spec.ts
+++ src/app/pds-cqs/views/todo-list-view/todo-list-view.component.spec.ts
@@ -1,21 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render, screen } from '@testing-library/angular';
+import { Todo } from '../../todo';
 import { TodoListViewComponent } from './todo-list-view.component';
 
 describe('TodoListViewComponent', () => {
   let component: TodoListViewComponent;
-  let fixture: ComponentFixture<TodoListViewComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [TodoListViewComponent]
-    });
-    fixture = TestBed.createComponent(TodoListViewComponent);
+  it('should create', async () => {
+    const { fixture } = await render(TodoListViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Input プロパティ todos の有無による表示の違い', () => {
+    it('todos を渡した場合に、checkbox が表示されること', async () => {
+      const todos: Todo[] = [{ id: 1, title: 'test', completed: false }];
+      const { fixture } = await render(TodoListViewComponent, {
+        componentInputs: { todos },
+      });
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      expect(screen.getByRole('checkbox', { suggest: true })).toBeTruthy();
+    });
+
+    it('todos を渡さなかった場合に、checkbox が表示されないこと', async () => {
+      const todos: Todo[] = [];
+      const { fixture } = await render(TodoListViewComponent, {
+        componentInputs: { todos },
+      });
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      expect(screen.queryByRole('checkbox', { suggest: true })).toBeNull();
+    });
   });
 });
