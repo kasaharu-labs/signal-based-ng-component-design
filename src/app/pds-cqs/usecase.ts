dc03266dc4c161f4099285e4064a292ce0369b52
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 14:29:14 +0900

    feat: usecase 用意

---
+++ src/app/pds-cqs/usecase.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { TodoListState } from './state';
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoListUsecase {
+  #state = new TodoListState();
+
+  state = this.#state.asReadonly();
+
+  addTodo(title: string): void {
+    this.#state.addTodo(title);
+  }
+
+  setTodoCompleted(id: Todo['id'], completed: boolean) {
+    this.#state.setCompleted(id, completed);
+  }
+}
