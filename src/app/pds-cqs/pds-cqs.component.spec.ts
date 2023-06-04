c77f06692bb59782d8625bcc09706e38d01df832
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 16:06:23 +0900

    container のテストケースを追加

--- src/app/pds-cqs/pds-cqs.component.spec.ts
+++ src/app/pds-cqs/pds-cqs.component.spec.ts
@@ -1,21 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { routes } from '../app.routes';
 import PdsCqsComponent from './pds-cqs.component';
+import { TodoListUsecase } from './usecase';
 
 describe('PdsCqsComponent', () => {
+  let harness: RouterTestingHarness;
   let component: PdsCqsComponent;
-  let fixture: ComponentFixture<PdsCqsComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [PdsCqsComponent],
+      providers: [provideRouter(routes)],
     });
-    fixture = TestBed.createComponent(PdsCqsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    harness = await RouterTestingHarness.create();
+    component = await harness.navigateByUrl('', PdsCqsComponent);
+
+    harness.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('input に値を入力してボタンを押すと usecase.addTodo() が呼ばれること', () => {
+    const usecase = harness.routeDebugElement?.injector.get(TodoListUsecase)!;
+    spyOn(usecase, 'addTodo');
+    const hostElement: HTMLElement = harness.routeNativeElement!;
+    const inputElm = hostElement.querySelector('input')!;
+    inputElm.value = 'test';
+    inputElm.dispatchEvent(new Event('input'));
+
+    harness.detectChanges();
+
+    const buttonElm = hostElement.querySelector('button')!;
+    buttonElm.click();
+    expect(usecase.addTodo).toHaveBeenCalled();
   });
 });
