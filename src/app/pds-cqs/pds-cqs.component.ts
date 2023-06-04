8de800e45f5365ea32f553c468a0dac779ccb35b
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 14:48:01 +0900

    feat: todo list を実装

--- src/app/pds-cqs/pds-cqs.component.ts
+++ src/app/pds-cqs/pds-cqs.component.ts
@@ -1,11 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TodoListUsecase } from './usecase';
+import { TodoListViewComponent } from './views/todo-list-view/todo-list-view.component';
 
 @Component({
   selector: 'app-pds-cqs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, TodoListViewComponent],
+  providers: [TodoListUsecase],
   templateUrl: './pds-cqs.component.html',
   styleUrls: ['./pds-cqs.component.scss'],
 })
-export default class PdsCqsComponent {}
+export default class PdsCqsComponent {
+  usecase = inject(TodoListUsecase);
+
+  newTodoInput = new FormControl('', {
+    validators: [Validators.required],
+    nonNullable: true,
+  });
+
+  addTodo() {
+    const title = this.newTodoInput.value;
+    this.usecase.addTodo(title);
+    this.newTodoInput.reset();
+  }
+}
