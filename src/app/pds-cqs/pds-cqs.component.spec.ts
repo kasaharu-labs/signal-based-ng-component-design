79ae2bee54bd403866ae2914f639f06a25d9410d
Wataru.Kasahara <kasahara1019@gmail.com>
2023-06-04 13:18:25 +0900

    npm run ng g c pds-cqs

---
+++ src/app/pds-cqs/pds-cqs.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PdsCqsComponent } from './pds-cqs.component';
+
+describe('PdsCqsComponent', () => {
+  let component: PdsCqsComponent;
+  let fixture: ComponentFixture<PdsCqsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PdsCqsComponent]
+    });
+    fixture = TestBed.createComponent(PdsCqsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pds-cqs/pds-cqs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pds-cqs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pds-cqs.component.html',
+  styleUrls: ['./pds-cqs.component.scss']
+})
+export class PdsCqsComponent {
+
+}
